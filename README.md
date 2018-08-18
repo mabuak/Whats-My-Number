97943338c1f6c33f425488e67e2670d93f6ca0a0
Fachruzi Ramadhan <mabuak@live.com>
2018-08-18 16:24:56 +0700

    Add whats my number code.

---
+++ README.md
@@ -0,0 +1,16 @@
+# Whats My Number
+Simple IOS Project From Learning In Udemy
+
+This app storing data permanently
+
+Xcode 9.4.1 (Swift 4)
+
+## Screenshot
+
+![Whats-My-Number](https://github.com/mabuak/Whats-My-Number/blob/master/screenshot.png)
+
+## Links
+ - Apple Developer: [Swift Resources (Xcode 9 + Swift 4)](https://developer.apple.com/swift/resources/)
+ - Udemy Course: [Complete IOS 11 Developer Course](https://www.udemy.com/complete-ios-11-developer-course)
+
+*GNU General Public License v3.0*
--- Whats My Number/ViewController.swift
+++ Whats My Number/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var numberField: UITextField!
+    
+    @IBAction func save(_ sender: Any) {
+        UserDefaults.standard.set(numberField.text, forKey: "number")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let numberObject = UserDefaults.standard.object(forKey: "number")
+        
+        if let number = numberObject as? String {
+            numberField.text = number
+        }
     }
 
     override func didReceiveMemoryWarning() {
